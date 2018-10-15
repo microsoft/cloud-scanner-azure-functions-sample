8a42e7cb7a3586925a0d15108a7a861d1baabe62
Tanner Barlow <tabarlow@microsoft.com>
2018-10-15 18:16:44 +0000

    Merged PR 961: Integrate deployment from azure-resource-scanner repo into cloud-scanner-functions
    Related work items: #14524

---
+++ deployment/deployScript.ps1
@@ -0,0 +1,11 @@
+﻿Param( 
+   [string]$prefix = $(Read-Host "Provide company/group prefix"),
+   [string]$location = $(Read-Host "Provide location abbr"),
+   [string]$environment = $(Read-Host "Provider environment DEV|TEST|PROD"),
+   [string]$applicationName = $(Read-Host "Provider name for app")
+ )
+
+$resourceGroupName = $prefix + '-' + $location + '-' + $environment + '-' + $applicationName
+az group create -l westus -n $resourceGroupName
+
+az group deployment create --resource-group $resourceGroupName --name cloud-scanner --template-file deploy.azure.json  --parameters parameters.json
