1647e528468eaa35586a5fc107008c7b091445fd
Wallace Breza <wallace.breza@microsoft.com>
2018-10-24 10:35:18 -0700

    Setup travis build with pytest and flake8

--- RunRules/__init__.py
+++ RunRules/__init__.py
@@ -1,16 +1,19 @@
-from .. import settings # Loads all environment variables required
+# Loads all environment variables required
+from .. import settings
 import azure.functions
-
 from cloud_scanner.services import ResourceTagger
 
 # Load and register required services
 from cloud_scanner_azure import services
 from cloud_scanner_generic import services
 
+
 def main(timer: azure.functions.TimerRequest):
-    '''
+    """
     Runs rules as defined in ResourceTagger class. Currently disabled
+
     :param timer: Runs on timer as defined by function.json
     :return: None
-    '''
+    """
+
     ResourceTagger.process_tag_rules()
--- TaskScheduler/__init__.py
+++ TaskScheduler/__init__.py
@@ -1,13 +1,14 @@
 import azure.functions
+from .. import settings
 from cloud_scanner.services import TaskScheduler
 from cloud_scanner_azure import services
 from cloud_scanner_generic import services
 
-from .. import settings
 
 def main(timer: azure.functions.TimerRequest):
     """
     Schedule tasks for resource scanning on timed request
     :param timer: Timer defined by `function.json`
     """
+
     TaskScheduler.execute()
