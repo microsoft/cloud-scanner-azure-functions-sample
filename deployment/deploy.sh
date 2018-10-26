8de41ea3e424eaecc2d96bf41946548c398fcc1d
Wallace Breza <wallace@breza.me>
2018-10-26 15:14:54 -0700

    Updated travis deployment script (#14)
    * Updated travis deployment script
    
    * Changed to deploy on master

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,3 +1,3 @@
-az login --service-principal --username "$APP_ID" --password "$AZ_SP_SECRET" --tenant "$AZ_TENANT_ID"
+az login --service-principal --username "$AZ_APP_ID" --password "$AZ_SP_SECRET" --tenant "$AZ_TENANT_ID"
 az account get-access-token --query "accessToken"
 func azure functionapp publish $AZ_FUNCTIONS_APP_NAME --build-native-deps --force
